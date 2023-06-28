e47e29eca5765422db7373845070f3358b8dc4b4
Nguyen Thai Chau <chaunguyen4392@gmail.com>
2023-06-28 17:56:09 +0700

    Display YA-tokens image if it has ipfsHash

--- src/utils/asset.js
+++ src/utils/asset.js
@@ -1,6 +1,12 @@
 import cryptoassets from '@yaswap/wallet-core/dist/src/utils/cryptoassets'
 
 export const getAssetIcon = (asset, extension = 'svg') => {
+  const ipfsHash = cryptoassets[asset]?.ipfsHash
+  if (ipfsHash) {
+    console.log('TACA ===> getAssetIcon, asset = ', asset, 'ipfsHash = ', ipfsHash)
+    return `https://ipfs.io/ipfs/${ipfsHash}`
+  }
+
   const _asset = cryptoassets[asset]?.matchingAsset ?? asset
 
   try {
